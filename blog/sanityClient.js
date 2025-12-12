d43d7be2206d211f52b66966eda5010bf99816df
Anish Kshatriya <obliviga@gmail.com>
2025-12-12 13:33:10 -0800

    Update project ID

--- blog/sanityClient.js
+++ blog/sanityClient.js
@@ -1,8 +1,8 @@
-import { createClient } from 'https://cdn.skypack.dev/@sanity/client';
+import {createClient} from 'https://cdn.skypack.dev/@sanity/client'
 
 export const client = createClient({
-  projectId: '3ljyhtiw',
+  projectId: 'ufao1yzo',
   dataset: 'production',
   useCdn: true, // set to `false` to bypass the edge cache
-  apiVersion: '2023-05-03', // use current date (YYYY-MM-DD) to target the latest API version
-});
+  apiVersion: '2025-12-12', // use current date (YYYY-MM-DD) to target the latest API version
+})
--- sanity.cli.ts
+++ sanity.cli.ts
@@ -2,8 +2,8 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: '3ljyhtiw',
-    dataset: 'production'
+    projectId: 'ufao1yzo',
+    dataset: 'production',
   },
   deployment: {
     /**
@@ -11,5 +11,5 @@ export default defineCliConfig({
      * Learn more at https://www.sanity.io/docs/cli#auto-updates
      */
     autoUpdates: true,
-  }
+  },
 })
