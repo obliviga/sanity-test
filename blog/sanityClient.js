8687cee5c249ee6fab79dc3ece1fbf184333df42
Anish Kshatriya <obliviga@gmail.com>
2025-12-12 13:59:47 -0800

    remove old blog folder

--- blog/sanityClient.js
+++
@@ -1,8 +0,0 @@
-import {createClient} from 'https://cdn.skypack.dev/@sanity/client'
-
-export const client = createClient({
-  projectId: 'ufao1yzo',
-  dataset: 'production',
-  useCdn: true, // set to `false` to bypass the edge cache
-  apiVersion: '2025-12-12', // use current date (YYYY-MM-DD) to target the latest API version
-})
